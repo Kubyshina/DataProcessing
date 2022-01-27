b0d1969381baefdc8489f1c2651f449472f81f1d
Werecat <littleowl9@yandex.ru>
2022-01-28 00:43:43 +0300

    Home work 7

---
+++ Homework7/leroyparser/items.py
@@ -0,0 +1,17 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+from itemloaders.processors import TakeFirst
+
+
+class LeroyparserItem(scrapy.Item):
+    # define the fields for your item here like:
+    name = scrapy.Field(output_processor=TakeFirst())
+    article = scrapy.Field(output_processor=TakeFirst())
+    photos = scrapy.Field()
+    url = scrapy.Field(output_processor=TakeFirst())
+    price = scrapy.Field(output_processor=TakeFirst())
+    price = scrapy.Field(output_processor=TakeFirst())
